f64050ec6a3b5af62a5af1ce7d0a3eadc12bae1f
TheShadowblast123 <TheShadowblast123@yahoo.com>
2025-08-23 13:47:28 -0700

    adding cmp-adaptive-freq

---
+++ lua/plugins/cmp-adaptive-freq.lua
@@ -0,0 +1,7 @@
+return {
+	"TheShadowblast123/cmp-adaptive-freq",
+	dependencies = { "hrsh7th/nvim-cmp" },
+	config = function()
+		require("cmp-adaptive-freq").setup({})
+	end,
+}
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -43,6 +43,7 @@ return {
         { name = "luasnip" }, -- snippets
         { name = "buffer" }, -- text within current buffer
         { name = "path" }, -- file system paths
+		{ name = "cmp-adaptive-freq"}
       }),
       -- configure lspkind for vs-code like pictograms in completion menu
       formatting = {
